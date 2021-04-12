ed57481ca038e199396017eee124f570fe11206c
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-04-12 23:11:53 +0900

    21-04-12

---
+++ cpp 8.12/cpp 8.12/friend.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+class A; //foward declaraction
+
+class B
+{
+private:
+	int m_value = 2;
+public:
+
+	void doSomething(A& a);
+
+};
+
+
+class A
+{
+private:
+	int m_value = 1;
+
+	//friend void doSomething(A& a, B& b); // private이지만 friend로 설정해 주었기 때문에 접근이 가능하다.
+
+	//friend class B;
+	friend void B::doSomething(A& a);
+
+};
+
+
+void B::doSomething(A& a) {
+	cout << a.m_value << endl;
+}
+
+int main()
+{
+	A a;
+	B b;
+	b.doSomething(a);
+
+	return 0;
+}

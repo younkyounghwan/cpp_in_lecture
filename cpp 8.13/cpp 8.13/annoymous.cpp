c60f36821ecd556305afb00d01e6152639c84ff3
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-04-16 23:23:43 +0900

    21-04-16

--- cpp 8.13/cpp 8.13/annoymous.cpp
+++ cpp 8.13/cpp 8.13/annoymous.cpp
@@ -25,6 +25,24 @@ public:
 	}
 };
 
+class Cents
+{
+private:
+	int m_cents;
+
+public:
+	Cents(int cents) { m_cents = cents; }
+
+	int getCents() const { return m_cents; }
+
+
+};
+
+Cents add(const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.getCents() + c2.getCents());
+}
+
 int main()
 {
 	A a(1);
@@ -34,6 +52,11 @@ int main()
 
 	//A().print(); //A()가 Lvalue가 아닌 Rvalue로 작동한다.
 	//A().print();
+
+
+	cout << add(Cents(6), Cents(8)).getCents() << endl;
+	cout << int(6) + int(8) << endl; //operating overloading
+
 
 	return 0;
 }
---
+++ cpp 8.14/cpp 8.14/nested types.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Fruit
+{
+public:
+	enum class FruitType
+	{
+		APPLE, BANANA, CHERRY
+
+	};
+
+	class InnerClass
+	{
+
+	};
+
+	struct InnerStruct
+	{
+
+	};
+private:
+	FruitType m_type;
+public:
+	Fruit(FruitType type) : m_type(type)
+	{}
+
+	FruitType getType() { return m_type; }
+};
+
+int main()
+{
+	Fruit apple(Fruit::FruitType::APPLE);
+	 if (apple.getType() == Fruit::FruitType::APPLE)
+	{
+		cout << "Apple" << endl;
+	}
+	return 0;
+}

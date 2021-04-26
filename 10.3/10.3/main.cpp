314f5d07b974423705eb71eff3b27d25d3b22e63
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-04-26 23:47:33 +0900

    21-04-26

---
+++ 10.3/10.3/main.cpp
@@ -0,0 +1,17 @@
+// 집합관계
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "lecture.h"
+
+
+using namespace std;
+
+//composition Relationship
+Lecture 
+
+int main()
+{
+	return 0;
+}

8d8cc5f52f8c149101175ea73ed301b55b9eb4ad
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-04-30 17:14:59 +0900

    21-04-30

---
+++ cpp 10.4/cpp 10.4/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Patient
+{
+private:
+	string m_name;
+	/*vector<Doctor*> m_doctors;*/
+
+public:
+	Patient(string name_in)
+		: m_name(name_in)
+	{}
+
+};
+
+class Doctor
+{
+private:
+	string m_name;
+
+public:
+	Doctor(string name_in)
+		: m_name(name_in)
+	{}
+};
+int main()
+{
+	Patient* p1 = new Patient("Jack Jack");
+	return 0;
+}

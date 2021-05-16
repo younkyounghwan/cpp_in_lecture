28e41ca09a6d21bca3f314e45ec0984d29b688f2
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-05-16 23:10:50 +0900

    21-05-16

--- cpp 12.2/cpp 12.2/main.cpp
+++ cpp 12.2/cpp 12.2/main.cpp
@@ -2,8 +2,40 @@
 
 using namespace std;
 
+class A
+{
+public:
+	void print() { cout << "A" << endl; }
+};
+
+class B : public A
+{
+public:
+	void print() { cout << "B" << endl; }
+};
+
+class C : public B
+{
+public:
+	void print() { cout << "C" << endl; }
+};
+
+class D : public C
+{
+public:
+	void print() { cout << "D" << endl; }
+};
+
 int main()
 {
+
+	A a;
+	B b;
+	C c;
+	D d;
+	
+	A& ref = b; // output: A
+	ref.print();
 
 	return 0;
 }

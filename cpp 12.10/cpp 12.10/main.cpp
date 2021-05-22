634eaed2a8d73f9b8e318fc30383ca5c673d9a16
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-05-22 23:53:55 +0900

    21-05-22

---
+++ cpp 12.10/cpp 12.10/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+// 천천히 음미하면서 이번 강의를 들어보자 
+// 코드의 의도를 마치 생명의 숨결처럼 느끼면서 코드를 읽어보자
+// 동적할당을 주로 사용하지는 않는다.
+// 한번쯤은 알아두면 객체지향을 이해하는데에 도움이 된다.
+
+
+using namespace std;
+
+class Base
+{
+public:
+	int m_i = 0;
+
+	virtual void print()
+	{
+		cout << "I'm Base" << endl;
+	}
+};
+
+class Derived1 : public Base
+{
+public:
+	int m_j = 1024;
+
+	virtual void print() override
+	{
+		cout << "I'm derived" << endl;
+	}
+};
+
+class Derived2 : public Base
+{
+public:
+	string m_name = "Dr. Two";
+
+	virtual void print() override
+	{
+		cout << "I'm derived" << endl;
+	}
+};
+
+
+int main()
+{
+	Derived1 d1;
+	Base* base = &d1;
+	d1.m_j;
+
+	// static_cast vs dynamic_cast 
+	auto* base_to_d1 = static_cast<Derived2*>(base); // dynamic_cast는 컴파일에 실패하면 null값을 넣어버린다.
+
+	if (&base_to_d1 != nullptr)
+		cout << base_to_d1->m_name << endl;
+	else 
+		cout << "Failed" << endl;
+
+	cout << base_to_d1->m_name << endl;
+	// base_to_d1->m_j = 256;
+
+	// cout << d1.m_j << endl;
+
+
+	return 0;
+}
--- cpp 12.9/cpp 12.9/main.cpp
+++ cpp 12.9/cpp 12.9/main.cpp
@@ -22,22 +22,36 @@ public:
 
 	virtual void print() override
 	{
-		cout << "I'm derived" << endl;
+		cout << "I'm Derived" << endl;
 	}
 };
 
 void doSomething(Base& b)
 {
-
+	b.print();
 }
 
 int main()
 {
+	Derived d1;
+	Base &b1 = d1; // &를 넣고 안넣고의 차이 
+	// Derived가 Base보다 정보량이 많기 때문에, Derived를 Base에 copy intialize로 할당하게 되면 데이터가 일부러 짤려나간다.
+
+	b1.print();
+
+	doSomething(d1);
+	
+	cout << endl;
+
+	Base b;
 	Derived d;
-	Base& b = d; // &를 넣고 안넣고의 차이
 
-	b.print();
+	vector<Base*> my_vec; // vector에는 reference를 넣을 수가 없다.
+	my_vec.push_back(&b); // reference를 굳이 사용하는 방법이 있는데 굳이 해야하나 싶다.
+	my_vec.push_back(&d); // Base에 의한 슬라이싱
 
+	for (auto& e : my_vec)
+		e->print();
 
 	return 0;
 }

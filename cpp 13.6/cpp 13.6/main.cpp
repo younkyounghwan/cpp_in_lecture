8475325b60565952db89fbf8e10ce4db43463bf7
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-01 23:07:43 +0900

    21-06-01

--- cpp 13.6/cpp 13.6/main.cpp
+++ cpp 13.6/cpp 13.6/main.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 template <class T, int size>
-
-class StaticArray
+class StaticArray_Base
 {
 private:
 	T m_array[size];
@@ -12,19 +11,48 @@ private:
 public:
 	T* getArray() { return m_array; }
 
-	T& operator [](int indexx)
+	T& operator [](int index)
 	{
 		return m_array[index];
 	}
+
+	
+	void print()
+	{
+		for (int count = 0; count < size; ++count)
+			cout << (*this)[count] << ' ';
+		cout << endl;
+	}
 };
 
-template <typename T, int size>
-void print(StaticArray<T, size>& array)
+template <class T, int size>
+class StaticArray : public StaticArray_Base<T, size>
 {
-	for (int count = 0; count < size; ++count)
-		cout << array[count] << ' ';
-	cout << endl;
-}
+public:
+	
+};
+template <int size>
+class StaticArray<char, size> : public StaticArray_Base<char, size>
+{
+public: 
+	void print()
+	{
+		for (int count = 0; count < size; ++count)
+			cout << (*this)[count];
+		cout << endl;
+	}
+};
+
+
+//template <int size>
+//void print(StaticArray<char, size>& array)
+//{
+//	for (int count = 0; count < size; ++count)
+//		cout << array[count];
+//	cout << endl;
+//}
+
+
 
 int main()
 {
@@ -34,16 +62,16 @@ int main()
 	int4[2] = 3;
 	int4[3] = 4;
 
-	print(int4);
+	int4.print();
 
 	StaticArray<char, 14> char14;
 	char14[0] = 'H';
 	char14[1] = 'e';
 
 	strcpy_s(char14.getArray(), 14, "Hello, World");
-	strcpy_s((char*)&char14[0], 14 * sizeof(char), "Hello, World");
+	//strcpy_s((char*)&char14[0], 14 * sizeof(char), "Hello, World");
 	
-	print(char14);
+	char14.print();
 	
 	return 0;
 }

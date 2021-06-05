3c14d2455da779b38f2fb202a0c73cc4c39f5dd3
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-05 23:58:41 +0900

    @1-06-05

--- cpp 13.8/cpp 13.8/main.cpp
+++ cpp 13.8/cpp 13.8/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream> 
+
 using namespace std;
+
+template<class T>
+class A {
+private:
+	T m_value;
+
+public:
+	A(const T& input)
+		: m_value(input)
+	{}
+
+	template<typename TT>
+	void doSomething(const TT & input)
+	{
+		cout << typeid(T).name() << " " << typeid(TT).name() << endl;
+		cout << (TT)m_value << endl;
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
 
 int main()
 {
+	A<int> a_int(123);
+	a_int.print();
+
+	a_int.doSomething('a');
 
 	return 0;
  }

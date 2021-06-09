733dd81b05b33398328415cd20aa81d04902a035
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-09 22:28:41 +0900

    21-06-09

--- cpp 14.2/cpp 14.2/main.cpp
+++ cpp 14.2/cpp 14.2/main.cpp
@@ -2,8 +2,83 @@
 
 using namespace std;
 
+// void last() throw(int) exception specifier
+// void last() throw(...) exception specifier
+ 
+void last() throw(...) //예외를 throw할 가능성이 있는 함수다 // parameter를 넣지 않으면 예외를 throw하지 않겠다는 뜻이 된다. 주의
+{
+	cout << "last " << endl;
+	cout << "Throws exception" << endl;
+
+	throw 'a';
+	
+	cout << "End last " << endl;
+}
+
+void third()
+{
+	cout << "Third" << endl;
+
+	last();
+
+	cout << "End third" << endl;
+}
+
+void second()
+{
+	cout << "Second" << endl;
+
+	try
+	{
+		third();
+	}
+	catch (double)
+	{
+		cerr << "Second caught double exception" << endl;
+	}
+
+	cout << "End second " << endl;
+}
+
+void first()
+{
+	cout << "first" << endl;
+
+	try
+	{
+		second();
+	}
+	catch (int)
+	{
+		cerr << "first caught int exception" << endl;
+	}
+
+	cout << "End first" << endl;
+
+}
+
 int main()
 {
+	cout << "Start" << endl;
+
+	try
+	{
+		first();
+	}
+	catch (int)
+	{
+		
+		cerr << "main caught int exception" << endl;
+	}
+
+	// uncaught exceptions
+
+	catch (...) // catch-all handlers
+	{
+		cerr << "main caught ellipses exception" << endl;
+	}
+
+	cout << "End main" << endl;
 
 
 	return 0;

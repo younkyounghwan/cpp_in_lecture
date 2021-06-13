72e795e7f10f76902ba7a78e77caab9feb159348
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-13 22:18:02 +0900

    21-06-13

--- cpp 14.4/cpp 14.4/main.cpp
+++ cpp 14.4/cpp 14.4/main.cpp
@@ -4,25 +4,30 @@
 
 using namespace std;
 
-//class CustomException : public exception
-//{
-//public:
-//	const char* what() const noexcept override
-//	{
-//		return "Custom exception";
-//	}
-//};
+class CustomException : public exception
+{
+public:
+	const char* what() const noexcept override // 다음 클래스를 exception의 자식 클래스로 쓰기 위해서 what()함수를 overriding해준다.
+	{ // Not to throw any exception
+		return "Custom exception";
+	}
+};
 
 int main()
 {
 	try
 	{
-		string s;
-		s.resize(-1);
+		/*string s;
+		s.resize(-1);*/
 
 		//throw runtime_error("Bad thing happened");
-		//throw CustomException();
+		throw CustomException();
 	}
+	//catch (length_error& exception) // 자식 클래스로 받기
+	//{
+	//	cerr << "Length_Error" << endl;
+	//	cerr << exception.what() << endl;
+	//}
 	catch (exception& exception)
 	{
 		cout << typeid(exception).name() << endl; // 자식 클래스를 보여준다

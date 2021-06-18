035fe50d6be0e5284f5bd6351cf6a8e13fd2370d
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-18 23:55:09 +0900

    21-06-18

--- cpp 15.2/cpp 15.2/main.cpp
+++ cpp 15.2/cpp 15.2/main.cpp
@@ -2,8 +2,50 @@
 
 using namespace std;
 
+int doSomething(int x)
+{
+	return x;
+}
+
+int getResult()
+{
+	return 100 * 100;
+}
+
 int main()
 {
+	int x = 5;
+	int y = getResult();
+	const int cx = 6;
+	const int cy = getResult();
+	
+	// L-value references
+
+	int& lr1 = x;         // Modifiable l-values
+	//int &lr2 = cx;      // Non-modifiable l-values
+	//int &lr3 = 5;       // R-values
+
+	const int& lr4 = x;   // Modifiable l-values
+	const int& lr5 = cx;  // Non-modifiable l-vlaues
+	const int& lr6 = 5;   // R-values // symbolic const 느낌으로 사용
+
+	// R-value references 
+
+	//int &&rr1 = x;      // Modifiable l-values
+	//int &&rr2 = cx;     // Non-modifiable l-values
+	int&& rr3 = 5;        // R-values 
+
+	//const int &&rr4 = x;  // Modifiable l-values
+	//const int &&rr5 = cx; // Non-modifiable l-values
+	const int&& rr6 = 5;    // R-values
+
+	// L/R-value reference parameters
+	doSomething(x);
+	// doSomething(cx);
+	doSomething(5);
+	doSomething(getResult());
+
+
 
 	return 0;
 }

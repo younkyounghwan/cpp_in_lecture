66805ed0f0782b22a6cecf28fb2c4537eca9fa7f
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-24 16:09:36 +0900

    21-06-24

--- cpp 15.4/cpp 15.4/main.cpp
+++ cpp 15.4/cpp 15.4/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
+	{
+		AutoPtr<Resource> res1(new Resource(10000000));
+
+		cout << res1.m_ptr << endl;
+
+		AutoPtr<Resource> res2 = res1;
+
+		cout << res1.m_ptr << endl;
+		cout << res2.m_ptr << endl;
+
+	}
 
 	return 0;
 }

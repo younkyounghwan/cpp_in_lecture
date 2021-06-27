2e9ec9ac06c0cf86d6661afc91b92f14f529cd14
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-06-27 23:55:24 +0900

    21-06-27

--- cpp 15.3/cpp 15.3/AutoPtr.h
+++ cpp 15.3/cpp 15.3/AutoPtr.h
@@ -22,32 +22,31 @@ public:
 	}
 
 
-	//AutoPtr(const AutoPtr& a)
-	//{
-	//	std::cout << "AutoPtr copy constructor " << std::endl;
+	AutoPtr(const AutoPtr& a) 
+	{
+		std::cout << "AutoPtr copy constructor " << std::endl;
 
-	//	// deep copy
-	//	m_ptr = new T;
-	//	*m_ptr = *a.m_ptr;
-	//	
-	//}
+		// deep copy
+		m_ptr = new T;
+		*m_ptr = *a.m_ptr;		
+	}
 
 
-	//AutoPtr& operator = (AutoPtr& a)
-	//{
-	//	std::cout << "AutoPtr copy assignment " << std::endl;
+	AutoPtr& operator = (AutoPtr& a)
+	{
+		std::cout << "AutoPtr copy assignment " << std::endl;
 
-	//	if (&a == this)      //prevent self-assignment
-	//		return *this;
+		if (&a == this)      //prevent self-assignment
+			return *this;
 
-	//	if (m_ptr != nullptr) delete m_ptr;
-	//	
-	//	//deep copy
-	//	m_ptr = new T;
-	//	*m_ptr = *a.m_ptr;
-	//
-	//	return *this;
-	//}
+		if (m_ptr != nullptr) delete m_ptr;
+		
+		//deep copy
+		m_ptr = new T;
+		*m_ptr = *a.m_ptr;
+	
+		return *this;
+	}
 
 	/*AutoPtr(const Auto& a) = delete;
 	AutoPtr& operator = (const AutoPtr& a) = delete;*/

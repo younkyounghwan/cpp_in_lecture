7430a2545f16e8958931e1cdc0e9e0c104dc8b06
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-07-07 22:10:30 +0900

    21-07-07

--- cpp 17.2/cpp 17.2/main.cpp
+++ cpp 17.2/cpp 17.2/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
+	string my_string;
 
+	string second_string(my_string);
+
+
+	cout << my_string << endl;
 	return 0;
 }

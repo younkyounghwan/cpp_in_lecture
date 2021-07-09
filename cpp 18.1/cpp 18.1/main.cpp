28ec3821fdfef2e2f4f6893e3d7a3c30c0992c81
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-07-09 23:33:14 +0900

    21-07-09

--- cpp 18.1/cpp 18.1/main.cpp
+++ cpp 18.1/cpp 18.1/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-
+#include <string>
+#include <iomanip> //input/ouput manipulators
 using namespace std;
 
 int main()
 {
+	char buf[5];
+
+	cin >> setw(5) >> buf;
+	cout << buf << endl;
+
+	cin >> setw(5) >> buf;
+	cout << buf << endl;
+
+	cin >> setw(5) >> buf;
+	cout << buf << endl;
+
+	cout << endl;
+
+	
+	int i;
+	float f;
+
+	cin >> i >> f;
+	cout << i << " " << f << endl;
+
+	char ch;
+
+	while (cin >> ch)
+		cout << ch; // ºóÄ­ÀÌ ¹«½ÃµÊ
+
+	while (cin.get(ch))
+		cout << ch; // ºóÄ­ÀÌ °í·ÁµÊ
+
 
 	return 0;
 }

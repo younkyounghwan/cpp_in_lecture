031bb64dc77921e836803f5efb10c2dacd48da9c
DESKTOP-5D4JF8N\youn7 <youn7117@naver.com>
2021-07-30 23:56:13 +0900

    21-07-30

--- cpp 20.1/cpp 20.1/main.cpp
+++ cpp 20.1/cpp 20.1/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <numeric> inner_product
 
 using namespace std;
 
+void func1()
+{
+    long long sum = 0;
+    for (unsigned i = 0; i < 1'000'000; ++i)
+        sum += i;
+
+    cout << sum << endl;
+}
+
+void func2()
+{
+    long long sum = 0;
+    for (unsigned i = 0; i < 1'000'000'000; ++i)
+        sum += i;
+
+    cout << sum << endl;
+}
+
+void run()
+{
+    func1();
+    func2();
+}
+
 int main()
 {
+    run();
 
-	return 0;
+    int* d = new int[1000];
+    float* f = new float[100];
+
+    delete[] d;
+    delete[] f;
+
+    return 0;
 }
